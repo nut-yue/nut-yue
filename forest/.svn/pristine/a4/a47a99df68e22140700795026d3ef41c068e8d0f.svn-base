914dcfbadf9c0a651f226f8e1f81548da4c978b1
nut-yue <y5786256@outlook.com>
2019-04-22 10:36:59 +0800

    nut-yue

---
+++ forest/.svn/pristine/a4/a47a99df68e22140700795026d3ef41c068e8d0f.svn-base
@@ -0,0 +1,10 @@
+$(function () {
+    $(".information").click(function () {
+        var table=layui.table;
+        var checkStatus=table.checkStatus('idTest');
+        var id=checkStatus.data[0].id;
+        console.log(checkStatus.data[0].id)
+        window.location.href="meeting.html?"+id;
+    })
+
+})
---
+++ forest/src/com/project/getSql/SelectSql.java
@@ -0,0 +1,61 @@
+package com.project.getSql;
+
+import java.util.Map;
+
+public class SelectSql {
+    public String getInsectSql(Map<String,String> codition){
+        String sql="SELECT id AS id," +
+                "insectName AS NAME," +
+                "hostName AS hostName," +
+                "breed AS breed," +
+                "enemy AS enemy," +
+                "controlling AS controlling," +
+                "danger AS danger," +
+                "childPicture AS childPicture," +
+                "insectPicture AS insectPicture\n" +
+                "FROM t_insect where 1=1 ";
+        String insectName=codition.get("insectName");
+        String hostName=codition.get("hostName");
+        if (insectName != null && insectName.length() != 0) {
+            sql+="and insectName like "+insectName;
+        }
+        if (hostName != null && hostName.length() != 0) {
+            sql+=" and hostName like "+hostName;
+        }
+        return sql;
+    }
+    public String getDiseaseSql(Map<String,String> condition){
+         String sql="SELECT id AS id, " +
+                 "diseaseName AS NAME," +
+                 "pathogeny AS pathogeny," +
+                 "symptom AS symptom," +
+                 "rule AS rule," +
+                 "mainDanger AS mainDanger," +
+                 "measure AS measure," +
+                 "picture AS picture FROM t_disease where 1=1 ";
+         String diseaseName=condition.get("diseaseName");
+         String mainDanger=condition.get("mainDanger");
+        if (diseaseName != null && diseaseName.length() != 0) {
+            sql+="and diseaseName like "+diseaseName;
+        }
+        if (mainDanger != null && mainDanger.length() != 0) {
+            sql+="and mainDanger like "+mainDanger;
+        }
+        return sql;
+    }
+    public String getMouseSql(Map<String,String> conditiion){
+        String sql="SELECT id AS id," +
+                "mouseName AS NAME," +
+                "food AS food," +
+                "breed AS breed," +
+                "enemy AS enemy," +
+                "measure AS measure," +
+                "mainDanger AS mainDanger," +
+                "picture AS picture FROM t_mouse where 1=1";
+        String name = conditiion.get("name");
+        if (name != null && name.length() != 0) {
+            sql+="mouseName like "+name;
+        }
+        return sql;
+    }
+}
