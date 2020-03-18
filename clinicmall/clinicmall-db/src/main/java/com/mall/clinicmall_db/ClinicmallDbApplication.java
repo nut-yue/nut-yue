bac3c956016152f2edfd5428b35b49c105b24f13
nut-yue <y5786256@outlook.com>
2020-03-18 22:29:14 +0800

    项目初始化

---
+++ clinicmall/clinicmall-db/src/main/java/com/mall/clinicmall_db/ClinicmallDbApplication.java
@@ -0,0 +1,13 @@
+package com.mall.clinicmall_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicmallDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicmallDbApplication.class, args);
+    }
+
+}
