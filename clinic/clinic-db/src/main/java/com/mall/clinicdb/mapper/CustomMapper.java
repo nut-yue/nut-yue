8626bbcecfde6b23a6a32469b70d821b8da185d7
nut-yue <y5786256@outlook.com>
2020-03-21 14:17:52 +0800

    del

--- clinic/clinic-db/src/main/java/com/mall/clinicdb/mapper/CustomMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mall.clinicdb.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mall.clinicdb.entity.CustomEntity;
-
-public interface CustomMapper extends BaseMapper<CustomEntity> {
-
-}
--- clinic/clinic-db/src/main/java/com/mall/clinicdb/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mall.clinicdb.mapper;
-
-/**
- * @ClassName : OrderMapper
- * @Date ：2020/1/14 11:03
- * @author：nut-yue
- */
-public interface OrderMapper {
-}
